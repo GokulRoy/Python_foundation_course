1a3991288ef24afa2af6c393dae4893849ba5a11
JCGokul <jangamchenchugokul@gmail.com>
2024-06-22 12:32:16 +0530

    second

---
+++ 2conversions/ASCII_values_conv.py
@@ -0,0 +1,7 @@
+a='A'
+ascii_a = ord(a)
+print(ascii_a)
+
+b=66
+ascii_b = chr(b)
+print(ascii_b)
---
+++ 2conversions/type_conv.py
@@ -0,0 +1,34 @@
+#implicit type conversion
+a=100
+b=1.5
+c=a+b
+print(type(c))
+print(c)
+
+# explicit type conversion
+d=200
+d=float(d)
+print(type(d))
+print(d)
+# f;oat
+num = 5+2.0
+print(type(num))
+# int
+num_int = int(num)
+print(type(num_int))
+print(num_int)
+# float
+num_float = float("3.14")
+print(type(num_float))
+print(num_float)
+# str
+message = str(42)
+print(type(message))
+print(message)
+# bool
+is_true = bool(1)
+print(type(is_true))
+print(is_true)
+is_false = bool(0)
+print(is_false)
+print(type(is_false))
